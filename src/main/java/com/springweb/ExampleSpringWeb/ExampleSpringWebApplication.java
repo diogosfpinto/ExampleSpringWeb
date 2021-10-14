91ab3ab723eb9a4de13b9222d933d174eddec7ec
diogosfpinto <diogo.sfpinto@gmail.com>
2021-10-13 20:01:32 -0400

    Minha primeira API usando Spring Web!

---
+++ src/main/java/com/springweb/ExampleSpringWeb/ExampleSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.springweb.ExampleSpringWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springweb/ExampleSpringWeb/ExampleSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springweb.ExampleSpringWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
